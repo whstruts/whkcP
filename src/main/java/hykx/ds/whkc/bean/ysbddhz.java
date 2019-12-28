bf31ee46ad2e1fe5c5a68ea67abe91d431bbece5
whstruts <whstruts@163.com>
2019-12-29 00:38:34 +0800

    20191205 whstruts 兵兵商品库存相关实体

---
+++ src/main/java/hykx/ds/whkc/bean/ysbddhz.java
@@ -0,0 +1,95 @@
+package hykx.ds.whkc.bean;
+
+public class ysbddhz {
+    private String djbh;
+    private String rq;
+
+    public String getDjbh() {
+        return djbh;
+    }
+
+    public void setDjbh(String djbh) {
+        this.djbh = djbh;
+    }
+
+    public String getRq() {
+        return rq;
+    }
+
+    public void setRq(String rq) {
+        this.rq = rq;
+    }
+
+    public String getOntime() {
+        return ontime;
+    }
+
+    public void setOntime(String ontime) {
+        this.ontime = ontime;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getDjje() {
+        return djje;
+    }
+
+    public void setDjje(double djje) {
+        this.djje = djje;
+    }
+
+    public String getXgdjbh() {
+        return xgdjbh;
+    }
+
+    public void setXgdjbh(String xgdjbh) {
+        this.xgdjbh = xgdjbh;
+    }
+
+    public String getBeizhu() {
+        return beizhu;
+    }
+
+    public void setBeizhu(String beizhu) {
+        this.beizhu = beizhu;
+    }
+
+    public String getIs_zx() {
+        return is_zx;
+    }
+
+    public void setIs_zx(String is_zx) {
+        this.is_zx = is_zx;
+    }
+
+    private String ontime;
+    private String customerId;
+    private String customerName;
+    private String status;
+    private double djje;
+    private String xgdjbh;
+    private String beizhu;
+    private String is_zx;
+}
---
+++ src/main/java/hykx/ds/whkc/bean/ysbddmx.java
@@ -0,0 +1,176 @@
+package hykx.ds.whkc.bean;
+
+public class ysbddmx {
+    private String djbh;
+    private String dj_sn;
+    private String goodscode;
+    private double num;
+    private double xsdj;
+    private double djzj;
+    private String batchcode;
+    private String validity;
+    private String status;
+    private String is_zx;
+
+    public String getDjbh() {
+        return djbh;
+    }
+
+    public void setDjbh(String djbh) {
+        this.djbh = djbh;
+    }
+
+    public String getDj_sn() {
+        return dj_sn;
+    }
+
+    public void setDj_sn(String dj_sn) {
+        this.dj_sn = dj_sn;
+    }
+
+    public String getGoodscode() {
+        return goodscode;
+    }
+
+    public void setGoodscode(String goodscode) {
+        this.goodscode = goodscode;
+    }
+
+    public double getNum() {
+        return num;
+    }
+
+    public void setNum(double num) {
+        this.num = num;
+    }
+
+    public double getXsdj() {
+        return xsdj;
+    }
+
+    public void setXsdj(double xsdj) {
+        this.xsdj = xsdj;
+    }
+
+    public double getDjzj() {
+        return djzj;
+    }
+
+    public void setDjzj(double djzj) {
+        this.djzj = djzj;
+    }
+
+    public String getBatchcode() {
+        return batchcode;
+    }
+
+    public void setBatchcode(String batchcode) {
+        this.batchcode = batchcode;
+    }
+
+    public String getValidity() {
+        return validity;
+    }
+
+    public void setValidity(String validity) {
+        this.validity = validity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getIs_zx() {
+        return is_zx;
+    }
+
+    public void setIs_zx(String is_zx) {
+        this.is_zx = is_zx;
+    }
+
+    public String getGoodsName() {
+        return GoodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        GoodsName = goodsName;
+    }
+
+    public String getSpecifications() {
+        return specifications;
+    }
+
+    public void setSpecifications(String specifications) {
+        this.specifications = specifications;
+    }
+
+    public String getApprove_number() {
+        return approve_number;
+    }
+
+    public void setApprove_number(String approve_number) {
+        this.approve_number = approve_number;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getProdDate() {
+        return prodDate;
+    }
+
+    public void setProdDate(String prodDate) {
+        this.prodDate = prodDate;
+    }
+
+    public String getScrq() {
+        return scrq;
+    }
+
+    public void setScrq(String scrq) {
+        this.scrq = scrq;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getSupplier_price() {
+        return supplier_price;
+    }
+
+    public void setSupplier_price(String supplier_price) {
+        this.supplier_price = supplier_price;
+    }
+
+    public String getJx() {
+        return jx;
+    }
+
+    public void setJx(String jx) {
+        this.jx = jx;
+    }
+
+    private String GoodsName;
+    private String specifications;
+    private String approve_number;
+    private String unit;
+    private String prodDate;
+    private String scrq;
+    private String manufacturer;
+    private String supplier_price;
+    private String jx;
+}
