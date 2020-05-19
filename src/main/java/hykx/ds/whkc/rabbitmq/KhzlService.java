badaafb14d7ab7315ead94f00e2fee0bbdcac647
whstruts <whstruts@163.com>
2020-05-20 02:24:11 +0800

    20200520 LM

--- src/main/java/hykx/ds/whkc/rabbitmq/KhzlService.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/KhzlService.java
@@ -1,14 +1,11 @@
 package hykx.ds.whkc.rabbitmq;
 
 import hykx.ds.whkc.bean.*;
-import hykx.ds.whkc.mapper.KhzlMapper;
+import hykx.ds.whkc.mapper.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class KhzlService {
@@ -35,54 +32,6 @@ public class KhzlService {
         return khzlMapper.getYZYGOODS_FIX(goods_sn);
     }
 
-    public List<BBSPJG> getBBSPJG() {
-        return  khzlMapper.getBBSPJG();
-    }
-
-    public List<BBSPKC> getBBSPKC() {
-        return  khzlMapper.getBBSPKC();
-    }
-
-    public List<BBSPPH> getBBSPPH() {
-        return  khzlMapper.getBBSPPH();
-    }
-
-    public List<BBSPZL> getBBSPZL() {
-        return  khzlMapper.getBBSPZL();
-    }
-
-    public List<Khzl> getKH() {
-        return  khzlMapper.getKH();
-    }
-
-    public List<Ddzt> getDD() {
-        return  khzlMapper.getDD();
-    }
-
-    public List<SpKC> getKC() {
-        return  khzlMapper.getKC();
-    }
-    public List<Spzl> getSP() { return khzlMapper.getSP();}
-    public void ItoDDHZs(ERPddhz ddhz){
-        khzlMapper.insertDDHZ(ddhz);
-    }
-    public void ItoDDMXs(ERPddmx ddmx){
-        khzlMapper.insertDDMX(ddmx);
-    }
-
-    public List<ERPddhz> getERPDD() {
-        return  khzlMapper.getERPDD();
-    }
-
-    public List<DZFP> getdzfp() {
-        return  khzlMapper.getdzfp();
-    }
-
-    public void DoERPDD(String kpbh,String cgjhbh,String xsjhbh) {
-        khzlMapper.DoERPDD(kpbh,cgjhbh,xsjhbh);
-    }
-
-   // public void updateERPDD(String kpbh) { khzlMapper.updateERPDD(kpbh);}
 
     public void updateysbddhz(String djbh) { khzlMapper.updateysbddhz(djbh);}
 
--- src/main/java/hykx/ds/whkc/rabbitmq/RabbitTopicConfig.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/RabbitTopicConfig.java
@@ -11,16 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitTopicConfig {
 
-    final static String WHKC = "topic.whkcx";
-    final static String WHKH = "topic.whkhcx";
-    final static String WHSP = "topic.whkspx";
-    final static String WHDD = "topic.whds";
-    final static String WHDZFP = "topic.whdzfp";
-
-    final static String BBSPZL = "topic.bbspzl"; //20191205 whstruts 兵兵 MSSQL 商品资料
-    final static String BBSPJG = "topic.bbspjg"; //20191205 whstruts 兵兵 MSSQL 商品价格
-    final static String BBSPKC = "topic.bbspkc"; //20191205 whstruts 兵兵 MSSQL 商品库存
-    final static String BBSPPH = "topic.bbspph"; //20191205 whstruts 兵兵 MSSQL 商品批号
 
     final static String YZYGOODS = "topic.yzygoods"; //20191226 whstruts 兵兵 MSSQL 商品库存
 
@@ -36,46 +26,6 @@ public class RabbitTopicConfig {
         return new Queue(RabbitTopicConfig.YZYGOODS);
     }
 
-    @Bean
-    public Queue queueBBSPZL() {
-        return new Queue(RabbitTopicConfig.BBSPZL);
-    }
-    @Bean
-    public Queue queueBBSPJG() {
-        return new Queue(RabbitTopicConfig.BBSPJG);
-    }
-    @Bean
-    public Queue queueBBSPKC() {
-        return new Queue(RabbitTopicConfig.BBSPKC);
-    }
-    @Bean
-    public Queue queueBBSPPH() {
-        return new Queue(RabbitTopicConfig.BBSPPH);
-    }
-    @Bean
-    public Queue queueWhkc() {
-        return new Queue(RabbitTopicConfig.WHKC);
-    }
-
-    @Bean
-    public Queue queueWhkh() {
-        return new Queue(RabbitTopicConfig.WHKH);
-    }
-
-    @Bean
-    public Queue queueWhsp() {
-        return new Queue(RabbitTopicConfig.WHSP);
-    }
-
-    @Bean
-    public Queue queueWhdd() {
-        return new Queue(RabbitTopicConfig.WHDD);
-    }
-
-    @Bean
-    public Queue queueWhdzfp() {
-        return new Queue(RabbitTopicConfig.WHDZFP);
-    }
 
     /**
      * 交换机(Exchange) 描述：接收消息并且转发到绑定的队列，交换机不存储消息
@@ -86,50 +36,6 @@ public class RabbitTopicConfig {
     }
     //綁定队列 queueYmq() 到 topicExchange 交换机,路由键只要是以 topic 开头的队列接受者可以收到消息
 
-    @Bean
-    Binding bindingExchangeBBSPZL(Queue queueBBSPZL, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueBBSPZL).to(topicExchange).with("topic.bbspzl");
-    }
-
-    @Bean
-    Binding bindingExchangeBBSPJG(Queue queueBBSPJG, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueBBSPJG).to(topicExchange).with("topic.bbspjg");
-    }
-
-    @Bean
-    Binding bindingExchangeBBSPKC(Queue queueBBSPKC, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueBBSPKC).to(topicExchange).with("topic.bbspkc");
-    }
-
-    @Bean
-    Binding bindingExchangeBBSPPH(Queue queueBBSPPH, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueBBSPPH).to(topicExchange).with("topic.bbspph");
-    }
-
-    @Bean
-    Binding bindingExchangeWhkc(Queue queueWhkc, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueWhkc).to(topicExchange).with("topic.whkcx");
-    }
-
-    @Bean
-    Binding bindingExchangeWhkh(Queue queueWhkh, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueWhkh).to(topicExchange).with("topic.whkhcx");
-    }
-
-    @Bean
-    Binding bindingExchangeWhsp(Queue queueWhsp, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueWhsp).to(topicExchange).with("topic.whkspx");
-    }
-
-    @Bean
-    Binding bindingExchangeWhdd(Queue queueWhdd, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueWhdd).to(topicExchange).with("topic.whds");
-    }
-
-    @Bean
-    Binding bindingExchangeWhdzfp(Queue queueWhdzfp, TopicExchange topicExchange) {
-        return BindingBuilder.bind(queueWhdzfp).to(topicExchange).with("topic.whdzfp");
-    }
 
     @Bean
     Binding bindingExchangeYZYGOODS(Queue queueYZYGOODS, TopicExchange topicExchange) {
