d894d73e7516ad24608ed38c29d166e6537dab8b
whstruts <whstruts@163.com>
2020-05-20 16:38:07 +0800

    20200520 LM  完成发布订阅功能

--- src/main/java/hykx/ds/whkc/rabbitmq/Listener.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/Listener.java
@@ -1,17 +1,51 @@
 package hykx.ds.whkc.rabbitmq;
 
+import hykx.ds.whkc.bean.YZYGOODS;
+import hykx.ds.whkc.tools.JSONChange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class Listener {
+    @Autowired
+    private KhzlService khzlService;
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RabbitListener(queues = "${rabbitmq.fanout.queue.b}")
-    public void handleQueue(String content) {
-        logger.info("handleQueue:{} {}", content, Thread.currentThread().getId());
+    public void handleQueue(String content) throws IOException {
+        int i_pos;
+        int i_pos2;
+        String s_json;
+        YZYGOODS yzygoods = new YZYGOODS();
+        i_pos = content.indexOf("DELETEYZYGOODS");
+        i_pos2 = content.indexOf("UPDATEYZYGOODS");
+        if(i_pos>0){
+            khzlService.deleteYZYGOODS();
+            System.out.println("接收者 message," + content);
+        }else if(i_pos2>0){
+            khzlService.updateYZYGOODS();
+            System.out.println("接收者 message," + content);
+        }
+        else {
+            i_pos = content.indexOf("{");
+            s_json = content.substring(i_pos);
+            yzygoods = (YZYGOODS) JSONChange.jsonToObj(yzygoods, s_json);
+            khzlService.insertYZYGOODS(yzygoods);
+            System.out.println("接收者 TopicReceiverYZYGOODS," + s_json);
+
+            List<YZYGOODS> yzygoods_fix_list = khzlService.getYZYGOODS_FIX(yzygoods.getGoods_sn());
+
+            if(yzygoods_fix_list.size()==0)
+            {
+                khzlService.insertYZYGOODS_FIX(yzygoods);
+            }
+        }
     }
 }
--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -1,11 +1,17 @@
 package hykx.ds.whkc.rabbitmq;
 
 
+import hykx.ds.whkc.bean.ysbdd;
+import hykx.ds.whkc.bean.ysbddhz;
+import hykx.ds.whkc.bean.ysbddmx;
+import net.sf.json.JSONObject;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
+import java.util.List;
+
 @Component
     public class ScheduledTasks {
         @Autowired
@@ -13,38 +19,38 @@ import java.text.SimpleDateFormat;
         @Autowired
         private KhzlService khzlService;
         private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-//        @Scheduled(fixedDelay = 60*1000)
-//        public void reportCurrentTime()throws Exception {
-//        List<ysbddhz> listysbddhz = khzlService.getysbddhzs();
-//        for (int i = 0; i < listysbddhz.size(); i++) {
-//             ysbddhz ddhz = listysbddhz.get(i);
-//             List<ysbddmx> listDDMX = khzlService.getysbddmxbydjbh(ddhz.getDjbh());
-//             ysbdd dd = new ysbdd();
-//            if(listDDMX.size()>0)
-//            {
-//                dd.setYsbddhz(ddhz);
-//                dd.setYsbddmxes(listDDMX);
-//            }
-//            else
-//                return;
-//            khzlService.updateysbddhz(ddhz.getDjbh());//更新订单汇总状态
-//
-//            JSONObject data = JSONObject.fromObject(dd);
-//
-//            System.out.println("GetDD,Name:" + data.toString());
-//
-//            String context = data.toString();
-//
-//            String routeKey = "topic.ysbdd";
-//
-//            String exchange = "topicExchange";
-//
-//            context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
-//
-//            System.out.println("sendDDTest : " + context);
-//
-//            this.rabbitTemplate.convertAndSend(exchange, routeKey, context);
-//        }
-//    }
+        @Scheduled(fixedDelay = 60*1000)
+        public void reportCurrentTime()throws Exception {
+        List<ysbddhz> listysbddhz = khzlService.getysbddhzs();
+        for (int i = 0; i < listysbddhz.size(); i++) {
+             ysbddhz ddhz = listysbddhz.get(i);
+             List<ysbddmx> listDDMX = khzlService.getysbddmxbydjbh(ddhz.getDjbh());
+             ysbdd dd = new ysbdd();
+            if(listDDMX.size()>0)
+            {
+                dd.setYsbddhz(ddhz);
+                dd.setYsbddmxes(listDDMX);
+            }
+            else
+                return;
+            khzlService.updateysbddhz(ddhz.getDjbh());//更新订单汇总状态
+
+            JSONObject data = JSONObject.fromObject(dd);
+
+            System.out.println("GetDD,Name:" + data.toString());
+
+            String context = data.toString();
+
+            String routeKey = "topic.ysbdd";
+
+            String exchange = "topicExchange";
+
+            context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+
+            System.out.println("sendDDTest : " + context);
+
+            this.rabbitTemplate.convertAndSend(exchange, routeKey, context);
+        }
+    }
 
 }
