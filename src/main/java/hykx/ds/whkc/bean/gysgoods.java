ce395aa4e39a51d394025dd8ad834df7ab17f515
whstruts <whstruts@163.com>
2020-07-24 09:43:07 +0800

    20200724 同步鑫海的商品数据到MQ（亿诺）

---
+++ src/main/java/hykx/ds/whkc/bean/gysgoods.java
@@ -0,0 +1,218 @@
+package hykx.ds.whkc.bean;
+
+public class gysgoods {
+
+
+    public void setGysbh(String gysbh) {
+        this.gysbh = gysbh;
+    }
+
+    public String getDrugCode() {
+        return drugCode;
+    }
+
+    public void setDrugCode(String drugCode) {
+        this.drugCode = drugCode;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getSptm() {
+        return sptm;
+    }
+
+    public void setSptm(String sptm) {
+        this.sptm = sptm;
+    }
+
+    public String getSpmch() {
+        return spmch;
+    }
+
+    public void setSpmch(String spmch) {
+        this.spmch = spmch;
+    }
+
+    public String getZjm() {
+        return zjm;
+    }
+
+    public void setZjm(String zjm) {
+        this.zjm = zjm;
+    }
+
+    public String getDw() {
+        return dw;
+    }
+
+    public void setDw(String dw) {
+        this.dw = dw;
+    }
+
+    public String getShpchd() {
+        return shpchd;
+    }
+
+    public void setShpchd(String shpchd) {
+        this.shpchd = shpchd;
+    }
+
+    public String getShpgg() {
+        return shpgg;
+    }
+
+    public void setShpgg(String shpgg) {
+        this.shpgg = shpgg;
+    }
+
+    public String getPizhwh() {
+        return pizhwh;
+    }
+
+    public void setPizhwh(String pizhwh) {
+        this.pizhwh = pizhwh;
+    }
+
+    public String getJixing() {
+        return jixing;
+    }
+
+    public void setJixing(String jixing) {
+        this.jixing = jixing;
+    }
+
+    public String getYouxq() {
+        return youxq;
+    }
+
+    public void setYouxq(String youxq) {
+        this.youxq = youxq;
+    }
+
+    public String getShangplx() {
+        return shangplx;
+    }
+
+    public void setShangplx(String shangplx) {
+        this.shangplx = shangplx;
+    }
+
+    public String getLeibie() {
+        return leibie;
+    }
+
+    public void setLeibie(String leibie) {
+        this.leibie = leibie;
+    }
+
+    public String getJlgg() {
+        return jlgg;
+    }
+
+    public void setJlgg(String jlgg) {
+        this.jlgg = jlgg;
+    }
+
+    public String getBzgg() {
+        return bzgg;
+    }
+
+    public void setBzgg(String bzgg) {
+        this.bzgg = bzgg;
+    }
+
+    public String getCunchtj() {
+        return cunchtj;
+    }
+
+    public void setCunchtj(String cunchtj) {
+        this.cunchtj = cunchtj;
+    }
+
+    public String getGmpzsyxq() {
+        return gmpzsyxq;
+    }
+
+    public void setGmpzsyxq(String gmpzsyxq) {
+        this.gmpzsyxq = gmpzsyxq;
+    }
+
+    public String getGmpzsh() {
+        return gmpzsh;
+    }
+
+    public void setGmpzsh(String gmpzsh) {
+        this.gmpzsh = gmpzsh;
+    }
+
+    public String getGsp_pzwhyxq() {
+        return gsp_pzwhyxq;
+    }
+
+    public void setGsp_pzwhyxq(String gsp_pzwhyxq) {
+        this.gsp_pzwhyxq = gsp_pzwhyxq;
+    }
+
+    public String getZzhcpj() {
+        return zzhcpj;
+    }
+
+    public void setZzhcpj(String zzhcpj) {
+        this.zzhcpj = zzhcpj;
+    }
+
+    public String getSsxkcy() {
+        return ssxkcy;
+    }
+
+    public void setSsxkcy(String ssxkcy) {
+        this.ssxkcy = ssxkcy;
+    }
+
+    public String getYsbgwj() {
+        return ysbgwj;
+    }
+
+    public void setYsbgwj(String ysbgwj) {
+        this.ysbgwj = ysbgwj;
+    }
+
+    public String getJingd() {
+        return jingd;
+    }
+
+    public void setJingd(String jingd) {
+        this.jingd = jingd;
+    }
+
+    private String gysbh;
+    private String drugCode;
+    private String stock;
+    private String sptm;
+    private String spmch;
+    private String zjm;
+    private String dw;
+    private String shpchd;
+    private String shpgg;
+    private String pizhwh;
+    private String jixing;
+    private String youxq;
+    private String shangplx;
+    private String leibie;
+    private String jlgg;
+    private String bzgg;
+    private String cunchtj;
+    private String gmpzsyxq;
+    private String gmpzsh;
+    private String gsp_pzwhyxq;
+    private String zzhcpj;
+    private String ssxkcy;
+    private String ysbgwj;
+    private String jingd;
+}
