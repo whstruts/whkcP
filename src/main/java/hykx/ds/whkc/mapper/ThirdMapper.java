146676496586ce19f7e329908d87cd5049b3a662
代世平 <daishiping@prolog-int.com>
2021-10-12 10:44:09 +0800

    20211012 程序已经可以取到数据库数据并发到MQ

--- src/main/java/hykx/ds/whkc/mapper/ThirdMapper.java
+++ src/main/java/hykx/ds/whkc/mapper/ThirdMapper.java
@@ -5,15 +5,51 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 public interface ThirdMapper {
-    @Select("select a.ddwname as Suppliers_name,b.prvno as ypdm,b.dspcd as cddm,c.jxmc as jx,isnull(d.scrq,'') as scrq," +
-            "       a.dspid+'_'+isnull(d.picih,'') as Goods_id_s,b.dspcode as Goods_sn,b.dspname as Drug_common_name,b.shenccj as manufacturer,c.pzwh as Approve_number,a.picurl as Drug_img,b.dspspgg as specifications," +
-            "       b.danwei as Package_unit,a.zhongbz as Medium_package,b.jianbz as Large_package,a.is_chail as Is_retail,isnull(d.picih,'') as production_batch,isnull(d.baozhiqi,'') as date_expiration,isnull(d.kcsl,0) as repertory," +
-            "       b.sptm as Bar_code,a.price as Supplier_price,a.is_zais as Is_on_sale,c.G_YPJYFW as Category,a.is_gengx from doc_sp_wz a" +
-            "                                                         left join doc_sp b on a.dspid=b.dspid" +
-            "                                                         left join doc_sp_gsp c on a.dspid=c.dspid" +
-            "                                                         left join (" +
-            "    select dspid,picih,scrq,baozhiqi,SUM(kcsl-wcsl) as kcsl from sp_kfpc" +
-            "    where kcsl-wcsl>0 group by dspid,picih,scrq,baozhiqi) d on a.dspid=d.dspid")
+    @Select("select " +
+            "a.code as drugid, " +
+            "a.spmc as drugCommonName ," +
+            "a.spellcode as nameZjm," +
+            "a.spcd as manufacturer," +
+            "a.spcdzjm as manufacturerZjm," +
+            "a.pzwh as approval," +
+            "'' as drugImg," +
+            "'' as recipeType," +
+            "'' as typeCode," +
+            "'' as dosageForm," +
+            "'' as appearance," +
+            "'' as bases," +
+            "'' as majorFunctions," +
+            "'' as untowardEffect," +
+            "'' as taboo," +
+            "'' as store," +
+            "'' as warnings," +
+            "'' as drugInteractions," +
+            "'' as brand," +
+            "'' as usageDosage," +
+            "a.spgg as specifications , " +
+            "a.unit as packageUnit," +
+            "'1' as mediumPackage," +
+            "a.spbz as largePackage," +
+            "'' as leftView," +
+            "'' as rightView," +
+            "'' as unpackView," +
+            "'' as specificationView," +
+            "a.code as goodsSn," +
+            "'1' as isRetail," +
+            "wm_concat(k.batchnumber) as productionBatch," +
+            "wm_concat(nvl(to_char(k.validdate,'yyyy-mm-dd'),'')) as dateExpriation," +
+            "nvl(sum(k.amount),0) as repertory ," +
+            "'' as barCode," +
+            "'安童生' as supplierName," +
+            "'' as supplier," +
+            "nvl(max(case when a.zdjj=0 then 9999 else a.zdjj end),9999)  as supplierPrice , " +
+            "wm_concat(nvl(to_char(k.productdate,'yyyy-mm-dd'),'')) as productionDate " +
+            "from zt_spzl a " +
+            "left join zt_kc k on k.code=a.code " +
+            "left join ZT_ZL_JYFW c on a.jyfw=c.code " +
+            "group by a.code,a.spmc,a.spgg,a.spcd,a.unit,a.pzwh,c.name,1,a.xxsl,a.spellcode,a.spcdzjm,a.spbz " +
+            "having sum(k.amount) > 0 " +
+            "order by drugid")
     public List<ThirdCommodity> getCommodity();
 
 }
