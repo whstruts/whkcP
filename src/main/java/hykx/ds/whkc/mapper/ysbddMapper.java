4b04a5b090c8f5807a8fba673d8327f3235930c7
代世平 <daishiping@prolog-int.com>
2023-03-29 23:15:56 +0800

    20230329 湖北东明 回写有华源品种的订单汇总状态为已锁定，回写华源品种的采购单价和采购金额

--- src/main/java/hykx/ds/whkc/mapper/ysbddMapper.java
+++ src/main/java/hykx/ds/whkc/mapper/ysbddMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Update;
 
 public interface ysbddMapper {
 
-    @Update("update ysb_ddmx set cgdj = #{cgdj},cgje = #{cgje} where djbh = #{djbh} and drugcode = #{drugcode}")
+    @Update("update ysb_ddmx set cg_dj = #{cgdj},cg_je = #{cgje} where djbh = #{djbh} and drugcode = #{drugcode}")
     public void updateddmx(ysbddmx ddmx);
 
     @Update("update ysb_ddhz set status = '已锁定' where djbh = #{djbh} ")
