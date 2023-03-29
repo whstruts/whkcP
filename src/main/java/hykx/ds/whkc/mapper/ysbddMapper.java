122ea2ee09758966f76556dcfa0c3850a7fc0d63
代世平 <daishiping@prolog-int.com>
2023-03-29 22:55:33 +0800

    20230329 湖北东明 回写有华源品种的订单汇总状态为已锁定，回写华源品种的采购单价和采购金额

---
+++ src/main/java/hykx/ds/whkc/mapper/ysbddMapper.java
@@ -0,0 +1,14 @@
+package hykx.ds.whkc.mapper;
+import hykx.ds.whkc.bean.ysbddhz;
+import hykx.ds.whkc.bean.ysbddmx;
+import org.apache.ibatis.annotations.Update;
+
+public interface ysbddMapper {
+
+    @Update("update ysb_ddmx set cgdj = #{cgdj},cgje = #{cgje} where djbh = #{djbh} and drugcode = #{drugcode}")
+    public void updateddmx(ysbddmx ddmx);
+
+    @Update("update ysb_ddhz set status = '已锁定' where djbh = #{djbh} ")
+    public void updateddhz(ysbddhz ddhz);
+
+}
---
+++ src/main/java/hykx/ds/whkc/rabbitmq/TopicReceiverHYDD.java
@@ -0,0 +1,45 @@
+package hykx.ds.whkc.rabbitmq;
+
+
+import hykx.ds.whkc.bean.ysbdd;
+import hykx.ds.whkc.bean.ysbddmx;
+import hykx.ds.whkc.mapper.ysbddMapper;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+@RabbitListener(queues = "topic.DMOrderBack")
+public class TopicReceiverHYDD {
+    @Autowired
+    ysbddMapper ysbddMapper;
+    @RabbitHandler
+    public void process(String message) throws IOException
+    {
+        int i_pos;
+        double total = 0.00;
+        String s_json;
+        i_pos = message.indexOf("{");
+        s_json = message.substring(i_pos);
+        JSONObject jsonObject = JSONObject.fromObject(s_json);
+        ysbdd dd = (ysbdd) JSONObject.toBean(jsonObject, ysbdd.class);
+        String aa = jsonObject.get("ysbddmxes").toString();
+        JSONArray array = JSONArray.fromObject(aa);
+        for(int i=0;i<array.size();i++)
+        {
+            ysbddmx mx = (ysbddmx) JSONObject.toBean(JSONObject.fromObject(array.get(i)), ysbddmx.class);
+            mx.setCgdj(mx.getCgdj());
+            mx.setCgje(mx.getCgje());
+            total = total + mx.getCgje();
+            ysbddMapper.updateddmx(mx);
+        }
+        ysbddMapper.updateddhz(dd.getYsbddhz());
+        System.out.println("接收者 TopicReceiverHYDD,"+s_json);
+    }
+}
