39be5f85f9f1dcb9f64420ebd5a27cb127f6d6a6
代世平 <daishiping@prolog-int.com>
2023-04-25 00:50:27 +0800

    20230424 HNYJ 增加批量同步更新商品数据的逻辑

---
+++ src/main/java/hykx/ds/whkc/rabbitmq/TopicReceiverYZYGOODSList2.java
@@ -0,0 +1,50 @@
+package hykx.ds.whkc.rabbitmq;
+
+import hykx.ds.whkc.bean.YZYGOODS;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONArray;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Component
+@RabbitListener(queues = "topic.HNYJGoodsList")
+public class TopicReceiverYZYGOODSList2 {
+    @Autowired
+    private KhzlService khzlService;
+    @RabbitHandler
+    public void process(String message) throws IOException
+    {
+        int i_pos;
+        int i_pos2;
+        String s_json;
+        YZYGOODS yzygoods = new YZYGOODS();
+        i_pos = message.indexOf("DELETEYZYGOODS");
+        i_pos2 = message.indexOf("UPDATEYZYGOODS");
+        if(i_pos>0){
+            khzlService.deleteYZYGOODS();
+            System.out.println("接收者 message," + message);
+        }else if(i_pos2>0){
+            khzlService.updateYZYGOODS();
+            System.out.println("接收者 message," + message);
+        }
+        else {
+            try {
+                i_pos = message.indexOf("[");
+                s_json = message.substring(i_pos);
+                List<YZYGOODS> goodsList = JSONArray.fromObject(s_json);
+                khzlService.batchUpdate(goodsList);
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.toString());
+                log.error("batchUpdate", e);
+            }
+        }
+    }
+}
