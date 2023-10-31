075928a7c98acc684b1b865c3f8462a65956ed0b
whstruts <whstruts@163.com>
2023-11-01 00:12:18 +0800

    20231031 湖北东明 增加并发接收及处理爬虫能力

---
+++ src/main/java/hykx/ds/whkc/rabbitmq/TopicReceiverYZYGOODSALLPP1.java
@@ -0,0 +1,49 @@
+package hykx.ds.whkc.rabbitmq;
+
+import hykx.ds.whkc.bean.YZYGOODS;
+import hykx.ds.whkc.tools.JSONChange;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RabbitListener(queues = "topic.HBDMGoodsAllPP")
+public class TopicReceiverYZYGOODSALLPP1 {
+    @Autowired
+    private KhzlService khzlService;
+    @RabbitHandler
+    public void process(String message) throws IOException
+    {
+        int i_pos;
+        int i_pos2;
+        String s_json;
+        YZYGOODS yzygoods = new YZYGOODS();
+        i_pos = message.indexOf("DELETEYZYGOODS");
+        i_pos2 = message.indexOf("UPDATEYZYGOODS");
+        if(i_pos>0){
+            khzlService.deleteYZYGOODS();
+            System.out.println("接收者 message," + message);
+        }else if(i_pos2>0){
+            khzlService.updateYZYGOODS();
+            System.out.println("接收者 message," + message);
+        }
+        else {
+            i_pos = message.indexOf("{");
+            s_json = message.substring(i_pos);
+            yzygoods = (YZYGOODS) JSONChange.jsonToObj(yzygoods, s_json);
+            khzlService.insertYZYGOODS(yzygoods);
+            System.out.println("接收者 TopicReceiverYZYGOODS," + s_json);
+
+            List<YZYGOODS>  yzygoods_fix_list = khzlService.getYZYGOODS_FIX(yzygoods.getGoods_sn());
+
+            if(yzygoods_fix_list.size()==0)
+            {
+                khzlService.insertYZYGOODS_FIX(yzygoods);
+            }
+        }
+    }
+}
