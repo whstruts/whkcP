b64c527c3937d84d1bb8e3fee700d1459514c3d6
whstruts <whstruts@163.com>
2024-06-12 23:04:06 +0800

    20240612 湖南源臻堂 剂型值为null的处理

--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -73,6 +73,8 @@ import lombok.extern.slf4j.Slf4j;
             System.out.println("取药帮忙数据:开始");
             List<MyGoodsEntity> list = MiddleService.GetYBMG2MGEByUser("HNYZT");
             for (MyGoodsEntity myGoodsEntity : list) {
+                if(myGoodsEntity.getJx()==null)
+                    myGoodsEntity.setJx("");
                 khzlService.insertYZYGOODS(myGoodsEntity);
             }
             System.out.println("取药帮忙数据:结束");
