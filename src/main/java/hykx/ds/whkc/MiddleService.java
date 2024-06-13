ebc9e18cc6926882eb059c7e9023d35aa1a1ca8a
whstruts <whstruts@163.com>
2024-06-13 16:27:49 +0800

    20240613 岳阳康尔 同步订单到RDS

--- src/main/java/hykx/ds/whkc/MiddleService.java
+++ src/main/java/hykx/ds/whkc/MiddleService.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import hykx.ds.whkc.entity.MyGoodsEntity;
 import hykx.ds.whkc.entity.YBMGoods;
 import hykx.ds.whkc.entity.YZYGOODS;
+import hykx.ds.whkc.entity.ysbdd;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,8 @@ public class MiddleService {
     public static final String MID_SYN_KC_URL = "http://116.62.46.187:10018/GetYZYGOODSByUser";
     public static final String MID_SYN_NC_KC_URL = "http://116.62.46.187:10018/GetNCGoods";
     public static final String MID_SYN_HYGY_KC_URL = "http://111.173.116.31:9020/GetHYGYAllWithSN";
+
+    public static final String MID_SYN_HYGY_ORDER_URL = "http://111.173.116.31:9020/saveOrderGY";
     public static List<YZYGOODS> GetYZYGOODSByUser(String userName) throws Exception {
         String param = "userName=" + userName;
         String res = HttpUtils.sendGet(MID_SYN_KC_URL, param);
@@ -35,6 +38,20 @@ public class MiddleService {
         JSONObject jsonObject = JSONObject.parseObject(res);
         List<YZYGOODS> yzygoodsList = jsonObject.getJSONArray("data").toJavaList(YZYGOODS.class);
         return yzygoodsList;
+    }
+
+
+    public static JSONObject saveOrder2GY(ysbdd order) throws Exception {
+        try{
+            String res = HttpUtils.getByBody(MID_SYN_HYGY_ORDER_URL,JSONObject.toJSONString(order));
+            JSONObject jsonObject = JSONObject.parseObject(res);
+            return jsonObject;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+
     }
 
 
