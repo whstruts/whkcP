0850b5c3a6ff49129084412763c153b8771c0e7d
whstruts <whstruts@163.com>
2024-06-18 23:00:27 +0800

    20240618 湖南益顺 解决按goods_sn来判断是否重复

--- src/main/java/hykx/ds/whkc/mapper/KhzlMapper.java
+++ src/main/java/hykx/ds/whkc/mapper/KhzlMapper.java
@@ -26,8 +26,8 @@ public interface KhzlMapper {
     @Select("select * from YZYGOODS_FIX where goods_sn = #{goods_sn}")
     public List<YZYGOODS> getYZYGOODS_FIX(String goods_sn);
 
-    @Select("select count(*) from YZYGOODS where ypbh = #{ypbh}")
-    public int getYZYGOODS(String ypbh);
+    @Select("select count(*) from YZYGOODS where goods_sn = #{goods_sn}")
+    public int getYZYGOODS(String goods_sn);
 
     @Update("update YZYGOODS set ypbh = #{ypbh},goods_name = #{goods_name},goods_number = #{goods_number},market_price = #{market_price},shop_price = #{shop_price},is_on_sale = 1,YPDM = #{YPDM},CDMC = #{CDMC},CDDM = #{CDDM},GG = #{GG},TXM = #{TXM},DW = #{DW},JX = #{JX},PZWH = #{PZWH},BZ = #{BZ},ZBZ = #{ZBZ},YXQ = #{YXQ}, PH = #{PH},ISRETAIL = #{ISRETAIL},PCH = #{PCH},SCRQ = #{SCRQ},ypbh = #{ypbh},updatetime = sysdate " +
             "where goods_sn = #{goods_sn}")
