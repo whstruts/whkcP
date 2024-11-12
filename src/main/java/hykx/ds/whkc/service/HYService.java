a90da3d3031fde91b66519f7e6c1eef85cb3803d
whstruts <whstruts@163.com>
2024-11-12 14:06:21 +0800

    20241112 聚鸣堂 取华源数据的服务IP及端口修改

--- src/main/java/hykx/ds/whkc/service/HYService.java
+++ src/main/java/hykx/ds/whkc/service/HYService.java
@@ -11,15 +11,15 @@ import java.util.List;
 @Slf4j
 @Service
 public class HYService {
-    public static final String HY_SYN_KC_URL = "http://47.96.253.115:9018/getHYGoods";
+    public static final String HY_SYN_KC_URL = "http://47.96.21.54:9020/getHYGoods";
     public static List<YZYGOODS> GetHYGoods() throws Exception {
         String res = HttpUtils.sendGet(HY_SYN_KC_URL,"");
         JSONObject jsonObject = JSONObject.parseObject(res);
         List<YZYGOODS> goodsList = jsonObject.getJSONArray("data").toJavaList(YZYGOODS.class);
         return goodsList;
     }
-//    public static void main(String[] args) throws Exception {
-//        GetHYGoods();
-//    }
+    public static void main(String[] args) throws Exception {
+        GetHYGoods();
+    }
 }
 
