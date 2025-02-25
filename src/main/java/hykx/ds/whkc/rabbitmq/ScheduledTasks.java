cc48a8ab18e9ef4a26006b23ce0d70901a5e1449
whstruts <whstruts@163.com>
2025-02-26 01:22:13 +0800

    20250226 湖北宁安堂 取爬虫数据

--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -40,16 +40,16 @@ import lombok.extern.slf4j.Slf4j;
             MiddleService.saveOrder2GY(dd);
         }
     }
-    @Scheduled(cron="0 0 1 * * ?")
-    private void DownDrug(){
-        try{
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            khzlService.unOnSale();
-            System.out.println(df.format(new Date()));
-        }catch (Exception e) {
-            log.error("全部华源商品下架", e);
-        }
-    }
+//    @Scheduled(cron="0 0 1 * * ?")
+//    private void DownDrug(){
+//        try{
+//            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            khzlService.unOnSale();
+//            System.out.println(df.format(new Date()));
+//        }catch (Exception e) {
+//            log.error("全部华源商品下架", e);
+//        }
+//    }
     @Scheduled(fixedDelay = 60*60*1000)
     public void reportCurrentTimeCommodityHYGY()throws Exception {
         try{
@@ -66,29 +66,44 @@ import lombok.extern.slf4j.Slf4j;
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             System.out.println(df.format(new Date()));
             System.out.println("取中台华源工业公司数据:结束");
+
+            System.out.println("取爬虫数据:开始");
+
+            List<YZYGOODS> list1 = MiddleService.GetPPGoods("HBNAT");
+
+            System.out.println("取爬虫数据:"+list1.size()+"行");
+
+            for(YZYGOODS yzygoods:list1)
+            {
+                khzlService.insertYZYGOODS(yzygoods);
+            }
+            SimpleDateFormat df1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println(df1.format(new Date()));
+            System.out.println("取爬虫数据:结束");
+
         }catch (Exception e) {
             log.error("reportCurrentTimeCommodityHYGY", e);
         }
     }
 
-    @Scheduled(fixedDelay = 60*60*1000)
-    public void reportCurrentTimeCommodityPP()throws Exception {
-        try{
-            System.out.println("取爬虫数据:开始");
-            List<YZYGOODS> list = MiddleService.GetPPGoods("HBNAT");
-
-            System.out.println("取爬虫数据:"+list.size()+"行");
-
-            for(YZYGOODS yzygoods:list)
-            {
-                khzlService.insertYZYGOODS(yzygoods);
-            }
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            System.out.println(df.format(new Date()));
-            System.out.println("取爬虫数据:结束");
-        }catch (Exception e) {
-            log.error("reportCurrentTimeCommodityPP", e);
-        }
-    }
+//    @Scheduled(fixedDelay = 60*60*1000)
+//    public void reportCurrentTimeCommodityPP()throws Exception {
+//        try{
+//            System.out.println("取爬虫数据:开始");
+//            List<YZYGOODS> list = MiddleService.GetPPGoods("HBNAT");
+//
+//            System.out.println("取爬虫数据:"+list.size()+"行");
+//
+//            for(YZYGOODS yzygoods:list)
+//            {
+//                khzlService.insertYZYGOODS(yzygoods);
+//            }
+//            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            System.out.println(df.format(new Date()));
+//            System.out.println("取爬虫数据:结束");
+//        }catch (Exception e) {
+//            log.error("reportCurrentTimeCommodityPP", e);
+//        }
+//    }
 
 }
