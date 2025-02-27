db5726d060cf3fd8d43fb667d56a075ade6a2c80
whstruts <whstruts@163.com>
2025-02-27 23:47:23 +0800

    20250227 湖南中欣致和 基本完成

--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
         List<ysbddhz> listysbddhz = khzlService.getysbddhzs();
         for (int i = 0; i < listysbddhz.size(); i++) {
             ysbddhz ddhz = listysbddhz.get(i);
-            ddhz.setUserName("HNDBT");
+            ddhz.setUserName("HNZXZH");
             List<ysbddmx> listDDMX = khzlService.getysbddmxbydjbh(ddhz.getDjbh());
             ysbdd dd = new ysbdd();
             if(listDDMX.size()>0)
@@ -89,7 +89,7 @@ import lombok.extern.slf4j.Slf4j;
     //@Scheduled(fixedDelay = 1000)
     public void reportCurrentTimeCommodityYBM()throws Exception {
         System.out.println("取中台数据:开始");
-        List<YZYGOODS> list = MiddleService.GetYZYGOODSByUser("HNDBT");
+        List<YZYGOODS> list = MiddleService.GetYZYGOODSByUser("HNZXZH");
         System.out.println("取到中台数据:"+list.size()+"行");
         for(YZYGOODS yzygoods:list)
         {
