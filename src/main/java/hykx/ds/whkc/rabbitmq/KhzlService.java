9c86272a3b56acabda9cc00b0934b21f833dfaf8
whstruts <whstruts@163.com>
2025-03-16 15:52:38 +0800

    20250316 江苏顺远 取裂变数据到ERP

--- src/main/java/hykx/ds/whkc/rabbitmq/KhzlService.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/KhzlService.java
@@ -97,5 +97,10 @@ public class KhzlService {
     }
 
     public void updateERPDD(String kpbh) { khzlMapper.updateERPDD(kpbh);}
-
+    public void deleteYZYGOODSP(){
+        khzlMapper.deleteYZYGOODSAllP();
+    }
+    public void insertYZYGOODSP(YZYGOODS yzygoods){
+        khzlMapper.insertYZYGOODSP(yzygoods);
+    }
 }
