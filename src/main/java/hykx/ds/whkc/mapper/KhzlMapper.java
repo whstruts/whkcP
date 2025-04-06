71595b5ea1dbe8587fa55db177f982855577507a
whstruts <whstruts@163.com>
2025-04-06 12:47:48 +0800

    20250406 壹铭尧盛 新增云仓商品裂变数据

--- src/main/java/hykx/ds/whkc/mapper/KhzlMapper.java
+++ src/main/java/hykx/ds/whkc/mapper/KhzlMapper.java
@@ -67,7 +67,7 @@ public interface KhzlMapper {
     void deleteYZYGOODSAllP();
 
     @Insert("INSERT INTO YZYGOODS_P(goods_sn,goods_name,goods_number,market_price,shop_price,is_on_sale,YPDM,CDMC,CDDM,GG,TXM,DW,JX,PZWH,BZ,ZBZ,YXQ,PH,ISRETAIL,PCH,SCRQ,goods_id_s,is_sy,ypbh,updatetime) "+
-            " VALUES(#{goods_sn},#{goods_name},#{goods_number},#{market_price},#{shop_price},#{is_on_sale},#{YPDM},#{CDMC},#{CDDM},#{GG},#{TXM},#{DW},#{JX},"+
+            " VALUES(#{goods_sn},#{goods_name},#{goods_number},#{market_price},#{shop_price},1,#{YPDM},#{CDMC},#{CDDM},#{GG},#{TXM},#{DW},#{JX},"+
             " #{PZWH},#{BZ},#{ZBZ},#{YXQ},#{PH},#{ISRETAIL},#{PCH},#{SCRQ},#{goods_id_s},'0',#{ypbh},#{updatetime})")
     void insertYZYGOODSP(YZYGOODS yzygoods);
 }
--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -95,8 +95,7 @@ import lombok.extern.slf4j.Slf4j;
         System.out.println("取中台裂变数据:开始");
         List<YZYGOODS> list = MiddleService.GetPGBYByUser("19079530276");
         System.out.println("取中台裂变数据:"+list.size()+"行");
-        int list_size = list.size();
-        if(list_size>0) khzlService.deleteYZYGOODSP();
+        khzlService.deleteYZYGOODSP();
         list.forEach(yzygoods -> {
             System.out.println("取中台裂变数据:" + yzygoods);
             khzlService.insertYZYGOODSP(yzygoods);
