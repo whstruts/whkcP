d1c46a945a88a611c61b0e74d7c48ae27cc7e0ee
whstruts <whstruts@163.com>
2025-04-11 03:17:55 +0800

    20250411 岳阳康尔 取小药药裂变数据异常处理

--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -50,32 +50,32 @@ import lombok.extern.slf4j.Slf4j;
             log.error("全部华源商品下架", e);
         }
     }
-    @Scheduled(fixedDelay = 60*60*1000)
-    //@Scheduled(fixedDelay = 1000)
-    public void reportCurrentTimeCommodityYBM()throws Exception {
-        System.out.println("取中台数据:开始");
-        List<YZYGOODS> list = MiddleService.GetYZYGOODSByUser("18692180722");
-        System.out.println("取到中台数据:"+list.size()+"行");
-        for(YZYGOODS yzygoods:list)
-        {
-            khzlService.insertYZYGOODS(yzygoods);
-        }
-        System.out.println("取中台数据:结束");
-    }
+//    @Scheduled(fixedDelay = 60*60*1000)
+//    //@Scheduled(fixedDelay = 1000)
+//    public void reportCurrentTimeCommodityYBM()throws Exception {
+//        System.out.println("取中台数据:开始");
+//        List<YZYGOODS> list = MiddleService.GetYZYGOODSByUser("18692180722");
+//        System.out.println("取到中台数据:"+list.size()+"行");
+//        for(YZYGOODS yzygoods:list)
+//        {
+//            khzlService.insertYZYGOODS(yzygoods);
+//        }
+//        System.out.println("取中台数据:结束");
+//    }
 
-    @Scheduled(fixedDelay = 60*60*1000)
-    //@Scheduled(fixedDelay = 1000)
-    public void reportCurrentTimeCommodityHYGY()throws Exception {
-        System.out.println("取中台华源工业公司数据:开始");
-        List<YZYGOODS> list = MiddleService.GetHYGYGoods("YYKR");
-
-        System.out.println("取中台华源工业公司数据:"+list.size()+"行");
-        for(YZYGOODS yzygoods:list)
-        {
-            khzlService.insertYZYGOODS(yzygoods);
-        }
-        System.out.println("取中台华源工业公司数据:结束");
-    }
+//    @Scheduled(fixedDelay = 60*60*1000)
+//    //@Scheduled(fixedDelay = 1000)
+//    public void reportCurrentTimeCommodityHYGY()throws Exception {
+//        System.out.println("取中台华源工业公司数据:开始");
+//        List<YZYGOODS> list = MiddleService.GetHYGYGoods("YYKR");
+//
+//        System.out.println("取中台华源工业公司数据:"+list.size()+"行");
+//        for(YZYGOODS yzygoods:list)
+//        {
+//            khzlService.insertYZYGOODS(yzygoods);
+//        }
+//        System.out.println("取中台华源工业公司数据:结束");
+//    }
 
     @Scheduled(fixedDelay = 60*60*1000)
     public void reportCurrentTimeCommodityPGBY()throws Exception {
@@ -86,7 +86,13 @@ import lombok.extern.slf4j.Slf4j;
         khzlService.deleteYZYGOODSP();
         list.forEach(yzygoods -> {
             System.out.println("批购包邮数据:" + yzygoods);
-            khzlService.insertYZYGOODSP(yzygoods);
+            try {
+                khzlService.insertYZYGOODSP(yzygoods);
+            }
+            catch (Exception e)
+            {
+                System.out.println("写入批购包邮数据异常:" + e.toString());
+            }
         });
         System.out.println("取批购包邮数据:结束");
     }
