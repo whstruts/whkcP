4acf1b7b5b8aa6339dea8351a21b6cb0e1a72a49
whstruts <whstruts@163.com>
2025-08-03 00:53:34 +0800

    20250803 湖北志远医药 取中台数据

--- src/main/java/hykx/ds/whkc/MiddleService.java
+++ src/main/java/hykx/ds/whkc/MiddleService.java
@@ -38,6 +38,6 @@ public class MiddleService {
         return yzygoodsList;
     }
     public static void main(String[] args) throws Exception {
-        GetPGBYByUser("HBHDYY");
+        GetPGBYByUser("HBZYYY");
     }
 }
--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
         List<ysbddhz> listysbddhz = khzlService.getysbddhzs();
         for (int i = 0; i < listysbddhz.size(); i++) {
             ysbddhz ddhz = listysbddhz.get(i);
-            ddhz.setUserName("HBHDYY");
+            ddhz.setUserName("HBZYYY");
             List<ysbddmx> listDDMX = khzlService.getysbddmxbydjbh(ddhz.getDjbh());
             ysbdd dd = new ysbdd();
             if(listDDMX.size()>0)
@@ -78,7 +78,7 @@ import lombok.extern.slf4j.Slf4j;
     @Scheduled(fixedDelay = 60*60*1000)
     public void reportCurrentTimeCommodityYBM()throws Exception {
         System.out.println("取中台数据:开始");
-        List<YZYGOODS> list = MiddleService.GetYZYGOODSByUser("HBHDYY");
+        List<YZYGOODS> list = MiddleService.GetYZYGOODSByUser("HBZYYY");
         System.out.println("取到中台数据:"+list.size()+"行");
         for(YZYGOODS yzygoods:list)
         {
@@ -90,7 +90,7 @@ import lombok.extern.slf4j.Slf4j;
     @Scheduled(fixedDelay = 60*60*1000)
     public void reportCurrentTimeCommodityPGBY()throws Exception {
         System.out.println("取中台裂变数据:开始");
-        List<YZYGOODS> list = MiddleService.GetPGBYByUser("HBHDYY");
+        List<YZYGOODS> list = MiddleService.GetPGBYByUser("HBZYYY");
         System.out.println("取中台裂变数据:"+list.size()+"行");
         khzlService.deleteYZYGOODSP();
         list.forEach(yzygoods -> {
