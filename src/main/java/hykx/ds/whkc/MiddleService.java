e9f8e84935a9e516dd873c60278198faa42a0eba
whstruts <whstruts@163.com>
2025-10-22 15:42:00 +0800

    20251022 湖北全杰 服务换IP

--- src/main/java/hykx/ds/whkc/MiddleService.java
+++ src/main/java/hykx/ds/whkc/MiddleService.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Service
 public class MiddleService {
     public static final String MID_SYN_KC_URL = "http://116.62.46.187:10018/GetYZYGOODSByUser";
-    public static final String MID_SYN_PGBY_KC_URL = "http://47.96.21.54:9021/GetAllPGBY";
+    public static final String MID_SYN_PGBY_KC_URL = "http://116.62.126.136:9021/GetAllPGBY";
 
     public static List<YZYGOODS> GetPGBY(String userName) throws Exception {
         String param = "userName=" + userName;
@@ -34,6 +34,6 @@ public class MiddleService {
     }
 
     public static void main(String[] args) throws Exception {
-        GetYZYGOODSByUser("HBQJ");
+        GetPGBY("HBQJ");
     }
 }
