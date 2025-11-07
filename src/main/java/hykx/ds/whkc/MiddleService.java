fff04e16c54f35fb74890657da34be479177c104
whstruts <whstruts@163.com>
2025-11-07 15:50:20 +0800

    20251107 江苏顺远 服务IP调整

--- src/main/java/hykx/ds/whkc/MiddleService.java
+++ src/main/java/hykx/ds/whkc/MiddleService.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Service
 public class MiddleService {
 
-    public static final String MID_SYN_PGBY_KC_URL = "http://116.62.46.187:9019/GetAllPGBY";
+    public static final String MID_SYN_PGBY_KC_URL = "http://112.124.67.70:9019/GetAllPGBY";
 
     public static List<YZYGOODS> GetPGBY(String userName) throws Exception {
         String param = "userName=" + userName;
