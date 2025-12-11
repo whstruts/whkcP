b21931abfcd7b338a0f3a682df4aa359b081f153
whstruts <whstruts@163.com>
2025-12-11 15:35:22 +0800

    20251211 湖北宁安堂 读取数据超时修改为30分钟

--- src/main/java/hykx/ds/whkc/HttpUtils.java
+++ src/main/java/hykx/ds/whkc/HttpUtils.java
@@ -27,7 +27,7 @@ public class HttpUtils
     // 连接超时时间（毫秒）
     private static final int CONNECT_TIMEOUT = 1000*10;
     // 读取超时时间（毫秒）
-    private static final int READ_TIMEOUT = 1000*60*10;
+    private static final int READ_TIMEOUT = 1000*60*30;
     /**
      * 向指定 URL 发送GET方法的请求
      *
